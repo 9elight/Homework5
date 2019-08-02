3ed62c625e542d4c7eea749c2ee22e90c18e71df
9elight <53308315+9elight@users.noreply.github.com>
2019-08-02 12:34:54 +0600

    fixed

--- src/com/company/MagicDoor.java
+++ src/com/company/MagicDoor.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class MagicDoor {
     public static Object magicDoor(){
-        Hero Warior = new Hero(200,25,100);
-        Hero Archer = new Hero(200, 30,60);
-        Hero Magic = new Hero(150,50,200);
-        Hero Lich = new Hero(200, 25,70);
-        Object[] Heroes = {Warior,Archer,Magic,Lich};
+        Hero warior = new Hero(200,25,100);
+        Hero archer = new Hero(200, 30,60);
+        Hero magic = new Hero(150,50,200);
+        Hero lich = new Hero(200, 25,70);
+        Object[] Heroes = {warior,archer,magic,lich};
 
         return Heroes;
     }
